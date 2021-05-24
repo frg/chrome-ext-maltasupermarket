fa710500f2f8936846274af23705d7c8c4ac902b
Jean Farrugia <jeanfrg@gmail.com>
2021-05-24 20:13:16 +0200

    fixed url change while in active tab bug

--- src/js/background.js
+++ src/js/background.js
@@ -7,27 +7,39 @@
  * Note that not all extensions need of a background.js file, but extensions that need to persist data after a popup has closed may need of it.
  */
 
-try {
-    // Set active icon when correct domain is active
-    chrome.tabs.onActivated.addListener((activeInfo) => {
-        console.info("🔰 Detected active tab change");
+// Set active icon when correct domain is active
+let currentUrl = null;
+const onTabUpdate = (url) => {
+    if (currentUrl === url) return;
+    currentUrl = url;
+
+    console.info(`🔰 Detected url change '${url}'`);
+
+    const parsedUrl = new URL(url);
+    const domainRegex = new RegExp(/(?:[a-zA-Z0-9]*\.)*([a-zA-Z0-9]+)\.[a-zA-Z0-9]+/, "g");
+    const domainMatch = [...(parsedUrl.host).matchAll(domainRegex)];
+    const domain = domainMatch.length === 0 ? parsedUrl.host : domainMatch[0][1] ?? parsedUrl.host;
+
+    console.info(`🔰 Parsed active domain: '${domain}'`);
         
+    if (domain === "maltasupermarket") {
+        chrome.action.setIcon({path: "/16x16.png"});
+    } else {
+        chrome.action.setIcon({path: "/grayscale-16x16.png"});
+    }
+};
+
+try {
+    chrome.tabs.onActivated.addListener((activeInfo) => {
+        console.info("🔰 Tab onActivated fired");
         chrome.tabs.get(activeInfo.tabId, async (tab) => {
-            console.info(`🔰 Active domain: '${tab.url}'`);
-
-            const url = new URL(tab.url);
-            const domainRegex = new RegExp(/(?:[a-zA-Z0-9]*\.)*([a-zA-Z0-9]+)\.[a-zA-Z0-9]+/, "g");
-            const domainMatch = [...(url.host).matchAll(domainRegex)];
-            const domain = domainMatch.length === 0 ? url.host : domainMatch[0][1] ?? url.host;
-
-            console.info(`🔰 Parsed active domain: '${domain}'`);
-            
-            if (domain === "maltasupermarket") {
-                chrome.action.setIcon({path: "/16x16.png"});
-            } else {
-                chrome.action.setIcon({path: "/grayscale-16x16.png"});
-            }
+            onTabUpdate(tab.url);
         });
+    });
+
+    chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+        console.info(`🔰 Tab onUpdated fired. Url: '${changeInfo.url}`);
+        onTabUpdate(changeInfo.url);
     });
 } catch (e) {
     // Without doing this, a failure on registration will give us no useful message
