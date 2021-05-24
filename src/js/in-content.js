9a3e06fbdcb3393faf8adb00524eb78d063069e9
Jean Farrugia <jeanfrg@gmail.com>
2021-05-24 11:52:01 +0200

    Removed unused code

--- src/js/in-content.js
+++ src/js/in-content.js
@@ -12,36 +12,6 @@ import * as selectors from "./lib/dom-selectors.js";
 import { getOrderSettingAsync, setOrderSetting } from "./lib/persistence.js";
 import { setSortOptionsSelection, generateSortOptionsEl } from "./lib/dom.js";
 
-// Extension port to communicate with the popup, also helps detecting when it closes
-let port = null;
-
-// Send messages to the open port (Popup)
-const sendPortMessage = data => port.postMessage(data);
-
-// Handle incoming popup messages
-const popupMessageHandler = message => console.log("in-content.js - message from popup:", message);
-
-// Start scripts after setting up the connection to popup
-chrome.runtime.onConnect.addListener(popupPort => {
-    // Listen for popup messages
-    popupPort.onMessage.addListener(popupMessageHandler);
-    // Set listener for disconnection (aka. popup closed)
-    popupPort.onDisconnect.addListener(() => {
-        console.log("in-content.js - disconnected from popup");
-    });
-    // Make popup port accessible to other methods
-    port = popupPort;
-    // Perform any logic or set listeners
-    sendPortMessage("message from in-content.js");
-});
-
-// Response handler for short lived messages
-const handleBackgroundResponse = response =>
-    console.log("in-content.js - Received response:", response);
-
-// Send a message to background.js
-chrome.runtime.sendMessage("Message from in-content.js!", handleBackgroundResponse);
-
 console.info("🔰 Loading");
 
 // If non existent append sort option to dropdown list. This is done through a listener because the element is dynamically generated.
