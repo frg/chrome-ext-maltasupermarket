efeec63d2cd16486a032bcdb59d3b46d9ba827ae
Jean Farrugia <jeanfrg@gmail.com>
2021-06-05 14:15:11 +0200

    Proper prod builds

--- webpack.common.js
+++ webpack.common.js
@@ -4,20 +4,16 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    mode: "production",
-
     entry: {
         popup: "./src/js/popup.js",
         background: "./src/js/background.js",
         "in-content": "./src/js/in-content.js",
     },
+
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].js",
     },
-
-    cache: true,
-    devtool: "inline-source-map",
 
     module: {
         rules: [
---
+++ webpack.dev.js
@@ -0,0 +1,10 @@
+/*global require, module*/
+
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+    mode: "development",
+    cache: true,
+    devtool: "inline-source-map",
+});
